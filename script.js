fd07d2a5f9cc89226ecba4609c8fa04111673de1
Wahab-dev09 <wahabzsec@gmail.com>
2024-10-21 01:10:40 -0600

    ch12

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
-const mian = document.querySelector('.main');
-const cursor = document.querySelector('.cursor');
-const image = document.querySelector('.image');
-
-// Move the cursor with the mouse on the main area
-mian.addEventListener('mousemove', (dets) => {
-  gsap.to(cursor, {
-    x: dets.x,
-    y: dets.y,
-  });
-});
-
-// Scale up the cursor when hovering over the image
-image.addEventListener('mousemove', () => {
-  cursor.innerHTML = "View more"
-  gsap.to(cursor, {
-    scale: 2,
-  });
-});
-
-// Reset the cursor when the mouse leaves the image
-image.addEventListener('mouseleave', () => {
-  cursor.innerHTML = ""
-  gsap.to(cursor, {
-    scale: 1,
-  });
-});
+const menu = document.querySelector("#menu")
+const close = document.querySelector("#close")
+const tl = gsap.timeline();
+tl.to(".drawer",{
+  right:0,
+  duration: 0.4,
+})
+tl.from(".drawer>h2",{
+  opacity:0,
+  x:100,  duration: 0.4,
+  stagger:0.3,
+})
+tl.from(".drawer>svg",{
+  opacity:0,
+  stagger:0.3,
+})
+tl.pause();
+menu.addEventListener('click', ()=>{
+  tl.play();
+})
+close.addEventListener('click', ()=>{
+  tl.reverse();
+})
