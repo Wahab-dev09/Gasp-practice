7f81e5cff420189882b63372d019c62c0c6efc36
Wahab-dev09 <wahabzsec@gmail.com>
2024-10-22 00:18:00 -0600

    ch13

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
-const menu = document.querySelector("#menu")
-const close = document.querySelector("#close")
-const tl = gsap.timeline();
-tl.to(".drawer",{
-  right:0,
+function BreakTheText(){
+  const h1 = document.querySelector('h1');
+const h1Text = h1.textContent;
+const splittedText = h1Text.split("");
+const halfvalue = splittedText.length/2;
+console.log(halfvalue)
+let clutter = "";
+splittedText.forEach((elem,idx)=>{
+  if(idx<halfvalue){
+    clutter += `<span class='a'>${elem}</span>`;
+  }else{
+    clutter += `<span class='b'>${elem}</span>`;
+  }
+});
+h1.innerHTML = clutter;
+console.log(clutter)
+}
+BreakTheText();
+gsap.from('h1 .a', {
+  opacity:0,
+  y: 30,
   duration: 0.4,
+  delay: 0.2,
+  stagger: 0.2,
 })
-tl.from(".drawer>h2",{
+gsap.from('h1 .b', {
   opacity:0,
-  x:100,  duration: 0.4,
-  stagger:0.3,
-})
-tl.from(".drawer>svg",{
-  opacity:0,
-  stagger:0.3,
-})
-tl.pause();
-menu.addEventListener('click', ()=>{
-  tl.play();
-})
-close.addEventListener('click', ()=>{
-  tl.reverse();
+  y: 30,
+  duration: 0.6,
+  delay: 0,
+  stagger: -0.25,
 })
